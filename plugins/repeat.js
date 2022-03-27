3be61cdf3f8adc406d7be509b964479ea5a02bc0
MIDU-SER <98685114+MIDU-SER@users.noreply.github.com>
2022-03-27 15:47:01 +0530

    update repeat.js

--- plugins/repeat.js
+++ plugins/repeat.js
@@ -1,1 +1,18 @@
+const Midu = require('../Utilis/events');
+const {MessageType} = require('@adiwajshing/baileys');
+const Config = require('../config');
+const fs = require('fs');
 
+const RE_DESC = "Repeat text simply."
+    
+    Midu.addCommand({pattern: 're', fromMe: true, desc: RE_DESC}, (async (message, match) => {    
+      const items = match.split(",")
+      if (items.length < 2) return await message.sendMessage("\n```Example .re hi,2");
+      const text = items[0];
+      const amo = items[1];
+      
+      const result = text.repeat(amo);
+      
+            await message.sendMessage(message.jid, result, MessageType.text);
+
+    }));
